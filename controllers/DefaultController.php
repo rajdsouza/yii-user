b5dd33e4f1250768883e9bb9500cf5f21b3c5301
Raj Dsouza <rajdsouza@gmail.com>
2015-01-30 16:27:01 +1100

    login method for api

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -1,25 +1,44 @@
 <?php
 
-class DefaultController extends Controller
-{
-	
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
-		$dataProvider=new CActiveDataProvider('User', array(
-			'criteria'=>array(
-		        'condition'=>'status>'.User::STATUS_BANNED,
-		    ),
-			'pagination'=>array(
-				'pageSize'=>Yii::app()->controller->module->user_page_size,
-			),
-		));
+class DefaultController extends Controller {
 
-		$this->render('/user/index',array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $dataProvider = new CActiveDataProvider('User', array(
+            'criteria' => array(
+                'condition' => 'status>' . User::STATUS_BANNED,
+            ),
+            'pagination' => array(
+                'pageSize' => Yii::app()->controller->module->user_page_size,
+            ),
+        ));
+
+        $this->render('/user/index', array(
+            'dataProvider' => $dataProvider,
+        ));
+    }
+
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return CMap::mergeArray(parent::filters(), array(
+                    'accessControl', // perform access control for CRUD operations
+        ));
+    }
+
+    public function accessRules() {
+        return array(
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('index'),
+                'users' => array('@'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
 
 }
